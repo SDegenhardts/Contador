63ac1e8ffc0055ffa5c4a262c71de53d1eb351a9
Santhiago Degenhardt <santhiago.dg@hotmail.com>
2023-05-17 17:17:07 -0300

    .

---
+++ .vscode/script.js/scri´t.js
@@ -0,0 +1,37 @@
+// var js = 'javascrisdhçgsdigjdjgkdj~gjdflgjçsfldpt'
+// console.log(js.toUpperCase())
+// var oi = 0
+// var oi1 = '0'
+
+// if(oi === oi1 && oi == oi1) {
+//     console.log('eles sao iguais')
+// } else {
+//     console.log('eles sao diferentes')
+// }
+
+// oi === oi1 ? console.log('é igual') : console.log('nao é igual')/*
+// switch (fdkfjsld) {
+//     case 2: {
+//         gsdjfgg
+//         break
+//     } 
+//     case 3 : {
+//         jfyjkkgtk
+//         break
+//     } 
+    
+// }
+
+// var loka = 1
+
+// while(loka <= 10) {
+//     console.log(`a var vale ${loka}`)
+//     loka++
+// } console.log('encerrou')
+
+// for(let loka = 1; loka <= 10; loka++) {
+//     console.log(`a var vale ${loka}`)
+// } console.log('encerrou')
+
+// var arrays = [10, 7, 2, 81, 32]
+// console.log(arrays[2])
